f1be56ff00f8228686f3c8c480b8f22e78fcd720
LereRentesca <esteban.renteria@uabc.edu.mx>
2024-10-15 13:13:28 -0700

    Class #2

---
+++ src/components/product.jsx
@@ -0,0 +1,24 @@
+import QuantityPicker from "./quantityPicker";
+import './styles/product.css';
+
+function Product(){
+    function add(){
+        console.log("adding to cart");
+    }
+
+    return(
+        <div className="product">
+            <img src="https://picsum.photos/250/200" alt=""/>
+            <h3>I'm a product</h3>
+
+            <label>$total</label>
+            <label>$price</label>
+
+            <QuantityPicker></QuantityPicker>
+
+            <button className="btn btn-sm btn-success" onClick={add}>Add</button> 
+        </div>
+    );
+}
+
+export default Product;
